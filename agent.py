4cb027f4c8b1056a6cdda83aec2fa739164f774a
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-03-23 22:20:45 +0530

    agent updated

--- agent.py
+++ agent.py
@@ -2,7 +2,7 @@ import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from crewai import Agent, Task, Crew, Process
 
-llm = ChatGoogleGenerativeAI(model="gemini-pro",verbose = True,temperature = 0.6,google_api_key="Aiekmasskeifhasskennvaldkeiahdklakn")
+llm = ChatGoogleGenerativeAI(model="gemini-pro",verbose = True,temperature = 0.6,google_api_key="")
 
 from langchain_community.tools import DuckDuckGoSearchRun
 
@@ -11,7 +11,7 @@ search_tool = DuckDuckGoSearchRun()
 researcher = Agent(
   role='Researcher',
   goal='You provide results on the basis of Facts and only Facts along with supported doc related urls,You go to the root cause and give the best possible outcomes',
-  backstory="You're an job research agent, you'll search for jobs available in market and those which are open now, avoid showing closed jobs",
+  backstory="You're a news agent, you'll provide the latest news and when it happened, be accurate and concise",
   verbose=True,
   allow_delegation=False,
   llm = llm,  
@@ -19,7 +19,7 @@ researcher = Agent(
 )
 
 task1 = Task(
-  description="I'm looking for react based openings",
+  description="""Recent trendy news""",
   agent=researcher,
   expected_output="give me clear and concise react openings"
 )
